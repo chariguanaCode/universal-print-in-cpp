ab1b8db8c958042dcd8ff634c93a71226d306fa0
Adam Jeliński <adas.jelinski@gmail.com>
2019-04-08 19:27:31 +0200

    added original debugging template

---
+++ main/template.cpp
@@ -0,0 +1,18 @@
+#include"bits/stdc++.h"
+using namespace std;
+
+#define debug if(1)
+#define watch(x) debug {cout << __LINE__ << ": " << (#x) << " is " << (x) << endl ;}
+#define watch_tab(x,n) debug {cout << __LINE__ << ": " << (#x) << " is "; \
+    for(int very_long_index=0;very_long_index<n;very_long_index++){ cout << x[very_long_index] << ", ";}\
+    cout << endl;}
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+}
