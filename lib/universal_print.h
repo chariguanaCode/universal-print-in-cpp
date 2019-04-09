1e68b75f57ae0bf6885dba517aa19c6df9a4b7c9
Adam Jeliński <adas.jelinski@gmail.com>
2019-04-09 17:43:09 +0200

    general cleanup

--- lib/universal_print.h
+++ lib/universal_print.h
@@ -1,12 +1,34 @@
-#define debug if(1)
-#define watch_tab(x,n) debug {cout << __LINE__ << ": " << (#x) << " is "; \
-    for(int very_long_index=0;very_long_index<n;very_long_index++){ cout << x[very_long_index] << ", ";}\
-    cout << endl;}
+/* ==============================================================================
+ *  
+ *  Contributors:
+ *      Name:       Adam Jeliński
+ *      Nickname:   charodziej
+ *
+ *  Description:    A library implementing a uniform method of printing variables in C++
+ *                  Mainly directed for competetive programming, to greatly speedup debugging
+ *
+ *  Created:        08.04.2019
+ *  Last updated:   09.04.2019
+ *
+ *  universal-print-in-cpp
+ *  Universal print in C++
+ *
+ *  g++ -std=c++11 -o template.o template.cpp
+ *  ./template.o
+ *
+ * ============================================================================== */
 
 #include<bits/stdc++.h>
+
+#define debug if(1)
+
+/* ------------------------------------------------------------------------------
+ *  Method of determining if a variable is iterable
+ *  thx to Jarod42
+ *  https://stackoverflow.com/questions/13830158/check-if-a-variable-is-iterable
+ * ------------------------------------------------------------------------------ */
 namespace detail
 {
-    // To allow ADL with custom begin/end
     using std::begin;
     using std::end;
 
@@ -27,51 +49,49 @@ namespace detail
 template <typename T>
 using is_iterable = decltype(detail::is_iterable_impl<T>(0));
 
+/* ------------------------------------------------------------------------------
+ *  Declaration of all the functions to prevent errors related to definition order
+ * ------------------------------------------------------------------------------ */
+template <typename T, typename std::enable_if<!(     is_iterable<T>::value||
+                                                std::is_pointer <T>::value),T>::type* =nullptr> void print_process(T &x);
+template <typename T, typename std::enable_if< (std::is_pointer <T>::value),T>::type* =nullptr> void print_process(T &x);
+template <typename T, typename std::enable_if< (     is_iterable<T>::value),T>::type* =nullptr> void print_process(T &x);
+
+#define array_extent_push(i) if(i<rank)sizes.push(std::extent<T,i>::value);
+template <typename T, typename std::enable_if<!(     is_iterable<T>::value),T>::type* =nullptr> void array_process(T &x, std::queue<int> sizes);
+template <typename T, typename std::enable_if< (     is_iterable<T>::value),T>::type* =nullptr> void array_process(T &x, std::queue<int> sizes);
+
+/* ------------------------------------------------------------------------------
+ *  The main definition called by the user from the main program
+ * ------------------------------------------------------------------------------ */
 #define watch(x)\
     std::cout << __LINE__ << ": " << #x << " is ";\
     print_process(x);\
     std::cout << '\n';
 
-template <typename T,
-          typename std::enable_if<!(is_iterable<T>::value||std::is_pointer<T>::value),T>::type* =nullptr>
-void print_process(T&x){
-    if(std::is_pointer<T>::value){
-        std::cout << "pointer" << std::endl;
-    } else {
-        std::cout << x << " ";
-    }
+/* ------------------------------------------------------------------------------
+ *  Printing of variables compatible with std::cout TODO: probably should check using is_arithmetic
+ * ------------------------------------------------------------------------------ */
+template <typename T, typename std::enable_if<!(     is_iterable<T>::value||
+                                                std::is_pointer <T>::value),T>::type* =nullptr>
+void print_process(T &x){
+    std::cout << x << " ";
 }
 
-template <typename T,
-          typename std::enable_if<(std::is_pointer<T>::value),T>::type* =nullptr>
+/* ------------------------------------------------------------------------------
+ *  Handling of pointers
+ * ------------------------------------------------------------------------------ */
+template <typename T, typename std::enable_if< (std::is_pointer <T>::value),T>::type* =nullptr>
 void print_process(T&x){
     print_process(*x);
 }
 
-
-template <typename T,
-          typename std::enable_if<!(is_iterable<T>::value),T>::type* =nullptr>
-void array_process(T &x, std::queue<int> sizes){
-    print_process(x);
-}
-
-template <typename T,
-          typename std::enable_if<(is_iterable<T>::value),T>::type* =nullptr>
-void array_process(T &x, std::queue<int> sizes){
-    std::cout << "{ ";
-    unsigned int n=sizes.front();
-    sizes.pop();
-    for (int i = 0; i < n; ++i) {
-        array_process(x[i],sizes);
-        if(sizes.size()>0)std::cout << '\n';
-    }
-    std::cout << "}";
-}
-
-#define array_extent_push(i) if(i<rank)sizes.push(std::extent<T,i>::value);
-
-template <typename T,
-          typename std::enable_if<is_iterable<T>::value,T>::type* =nullptr>
+/* ------------------------------------------------------------------------------
+ *  Iterating over iterable objects
+ *  std::rank returns the number of dimensions of a standard array, allowing us to process
+ *  multi dimensional arrays ( only the first level is iterable )
+ * ------------------------------------------------------------------------------ */
+template <typename T, typename std::enable_if< (     is_iterable<T>::value),T>::type* =nullptr>
 void print_process(T&x){
     if(std::rank<T>::value){
         std::queue<int>sizes;
@@ -87,5 +107,28 @@ void print_process(T&x){
         }
         std::cout << "}";
     }
+}
+
+/* ------------------------------------------------------------------------------
+ *  Recurrently go through all dimensions of an array
+ * ------------------------------------------------------------------------------ */
+template <typename T, typename std::enable_if<(      is_iterable<T>::value),T>::type* =nullptr>
+void array_process(T &x, std::queue<int> sizes){
+    std::cout << "{ ";
+    unsigned int n=sizes.front();
+    sizes.pop();
+    for (int i = 0; i < n; ++i) {
+        array_process(x[i],sizes);
+        if(sizes.size()>0)std::cout << '\n';
+    }
+    std::cout << "}";
+}
+
+/* ------------------------------------------------------------------------------
+ *  When reached the last dimension of an array, print its contents using previously defined methods
+ * ------------------------------------------------------------------------------ */
+template <typename T, typename std::enable_if<!(     is_iterable<T>::value),T>::type* =nullptr>
+void array_process(T &x, std::queue<int> sizes){
+    print_process(x);
 }
 
